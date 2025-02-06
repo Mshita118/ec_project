1aa3e8193549c893f854e8fd6930a612f8817bc8
Mshita118 <t.mshita118@gmail.com>
2025-02-06 09:58:34 +0900

    管理画面の作成

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    address = models.TextField(blank=True, null=True)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name="customuser_groups",
+        blank=True
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="customuser_permissions",
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
