9eacabc4f55e9b07e1f3ac36537ac554d2e84aba
Mshita118 <t.mshita118@gmail.com>
2025-02-06 09:35:03 +0900

    mysql設定

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ec_db',
+        'USER': 'mshita',
+        'PASSWORD': 'Tachi.0118',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
