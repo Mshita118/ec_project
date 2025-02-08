18a1d060d315becafee68a82c815c4e2292c1e47
Mshita118 <t.mshita118@gmail.com>
2025-02-08 22:55:42 +0900

    カートモデルの実装

--- cart/models.py
+++ cart/models.py
@@ -2,10 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from products.models import Product
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 8cb713d (カートモデルの作成)
 
 class CartItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -15,8 +11,3 @@ class CartItem(models.Model):
 
     def __str__(self):
         return f"{self.product.name} ({self.quantity})"
-<<<<<<< HEAD
-=======
->>>>>>> 49d6e58 (productの実装)
-=======
->>>>>>> 8cb713d (カートモデルの作成)
