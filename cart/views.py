7ef2f3201e74b8bc9870edf99669e7186b8f7e0b
Mshita118 <t.mshita118@gmail.com>
2025-02-08 23:17:55 +0900

    　cart/views.pyの実装

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, get_object_or_404
+from .models import CartItem
+from products.models import Product
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    cart_item, created = CartItem.objects.get_or_create(
+        user=request.user, product=product)
+
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+
+    return redirect('cart_detail')
