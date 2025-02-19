82178355139d50654980e5881984413a3f3d4285
Mshita118 <t.mshita118@gmail.com>
2025-02-19 23:24:26 +0900

    注文履歴の実装

--- orders/models.py
+++ orders/models.py
@@ -17,6 +17,8 @@ class OrderItem(models.Model):
         Order, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
 
     def __str__(self):
-        return f"{self.product.name} ({self.quantity})"
+        return f"{self.quantity} x {self.product.name}(Order {self.order.id})"
