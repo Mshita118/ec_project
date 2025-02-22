12e9514cd26cc43394511ac7bd28225b39c8c0f2
Mshita118 <t.mshita118@gmail.com>
2025-02-22 22:47:29 +0900

    ホーム画面の実装

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from products.views import home
 
 urlpatterns = [
+    path('', home, name='home'),
+    path('home/', home, name='home'),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('products/', include('products.urls')),
